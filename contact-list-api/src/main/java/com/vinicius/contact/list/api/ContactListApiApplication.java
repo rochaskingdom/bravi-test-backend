de67c33f2a4a870b9585435daf910c79bca22eee
Vinicius Rocha <viniciusr.rocha@outlook.com>
2022-10-03 18:30:05 -0300

    Criado módulo base contact-list-api

---
+++ contact-list-api/src/main/java/com/vinicius/contact/list/api/ContactListApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.contact.list.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactListApiApplication.class, args);
+    }
+
+}
---
+++ contact-list-api/src/test/java/com/vinicius/contact/list/api/ContactListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.contact.list.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
