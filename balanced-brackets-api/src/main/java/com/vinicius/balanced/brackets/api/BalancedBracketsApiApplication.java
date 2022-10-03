68bb1dcfb9f4674b164bbfbb8b393e9c453ed80f
Vinicius Rocha <viniciusr.rocha@outlook.com>
2022-10-03 18:19:07 -0300

    Criado modulo base balanced-brackets-api

---
+++ balanced-brackets-api/src/main/java/com/vinicius/balanced/brackets/api/BalancedBracketsApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.balanced.brackets.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancedBracketsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalancedBracketsApiApplication.class, args);
+    }
+
+}
