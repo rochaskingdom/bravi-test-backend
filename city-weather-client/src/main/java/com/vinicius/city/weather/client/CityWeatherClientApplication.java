bbb9694bcec2077797b4404011e1d6dc6a4b6b8e
Vinicius Rocha <viniciusr.rocha@outlook.com>
2022-10-03 20:28:20 -0300

    Criado modulo base city-weather-client

---
+++ city-weather-client/src/main/java/com/vinicius/city/weather/client/CityWeatherClientApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.city.weather.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityWeatherClientApplication.class, args);
+    }
+
+}
---
+++ city-weather-client/src/test/java/com/vinicius/city/weather/client/CityWeatherClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.city.weather.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityWeatherClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
